4bab75ba07cc555febf11ad4dc85a74440997ef1
Gokhan Derala <gderala@gmail.com>
2019-11-03 19:10:10 +0300

    posts routing added to blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,11 +15,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
+                            <li><a href="/posts">Posts</a></li>
                             <li><a href="/new-post">New Post</a></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/posts" exact render={() => <Posts/>} />
             </div>
         );
     }
