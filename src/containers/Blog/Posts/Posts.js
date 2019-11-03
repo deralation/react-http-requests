ee90fe0e456691144096e886c99bba8948ad9f64
Gokhan Derala <gderala@gmail.com>
2019-11-03 23:09:16 +0300

    load a single route using switch

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Post from './../../../components/Post/Post';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 import './Posts.css';
 
 class Posts extends Component{ 
@@ -33,9 +32,7 @@ class Posts extends Component{
     }
 
     postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
+        this.props.history.push({pathname: '/' + id});
     }
 
     render(){
@@ -45,14 +42,14 @@ class Posts extends Component{
         if(!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
+                    //<Link to={'/' + post.id} key={post.id}>
                         <Post
                             key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => this.postSelectedHandler(post.id)}
                         />
-                    </Link>
+                    //</Link>
                 );
             });
         }
