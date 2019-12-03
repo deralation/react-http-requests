9d6adb45e21ee499be8536713fc8ef73f10a54ef
gokhan derala <gderala@gmail.com>
2019-12-04 00:47:51 +0300

    nested routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 import { Route, NavLink, Switch } from 'react-router-dom';
-
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-
 import './Blog.css';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -18,7 +15,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                                to="/" 
+                                to="/posts/" 
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
@@ -34,10 +31,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
-                <Route path="/" exact component={Posts} />
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
